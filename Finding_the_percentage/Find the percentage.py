6271e7b2f50245543a7a61552fbaafc92b985423
Shruti <rajanishrutiln@gmail.com>
2019-02-11 21:11:24 -0600

    Challenege- Find the percentage

---
+++ Finding_the_percentage/Find the percentage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 21:07:16 2019
+
+@author: shrutirajani
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        record = input().split()
+        name, marks = record[0], record[1:]
+        marks = list(map(float, marks))
+        student_marks[name] = marks
+    query = input()
+    print('{0:.2f}'.format(*[(sum(scores)/len(scores)) for name, scores in 
+student_marks.items() if name == query]))
